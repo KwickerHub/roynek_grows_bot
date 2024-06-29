93308e6b4cb7cb4615593673cfe90e7a0fa1ace3
NtemKenyor <nkenyor@gmail.com>
2024-06-29 22:27:41 +0100

    on it o

--- get_webhook.py
+++ get_webhook.py
@@ -1,16 +1,6 @@
+
 import requests
-from dotenv import load_dotenv
-import os
-# Load environment variables from .env file
-load_dotenv()
-# BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Your webhook URL
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-# WEBHOOK_URL = 'https://your-app-url/webhook'
-# WEBHOOK_URL = "http://0.0.0.0:8000"
 
-url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook'
-data = {'url': WEBHOOK_URL}
-
-response = requests.post(url, data=data)
+bot_token = 'your_telegram_bot_token'
+response = requests.get(f'https://api.telegram.org/bot{bot_token}/getWebhookInfo')
 print(response.json())
---
+++ set_webhook.py
@@ -0,0 +1,16 @@
+import requests
+from dotenv import load_dotenv
+import os
+# Load environment variables from .env file
+load_dotenv()
+# BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Your webhook URL
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+WEBHOOK_URL = f'{WEBHOOK_URL}/webhook'
+# WEBHOOK_URL = "http://0.0.0.0:8000"
+
+url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook'
+data = {'url': WEBHOOK_URL}
+
+response = requests.post(url, data=data)
+print(response.json())
